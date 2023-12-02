29a57e633a88457a280f9c83839c969bddf4144e
RodrigoD4v <rodrigo20brbr@gmail.com>
2023-12-02 10:42:33 -0300

    Lógica das peças

---
+++ src/chessPieces/Knight.java
@@ -0,0 +1,13 @@
+package chessPieces;
+
+import boardGame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Knight extends ChessPiece{
+
+    public Knight(Board board, Color color) {
+        super(board, color);
+    }
+    
+}
